e817b3ad85fcdb3dece16cdf25318e6b8cae97f0
1131153523@qq.com <fanyihui@tuhu.cn>
2021-12-09 10:15:57 +0800

    changes

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -21,12 +21,6 @@ module.exports = {
         clientId: 'c31038f73d4704fa988c',
         clientSecret: '7d3a4b9d20632c4685b01cdcfaa05c5b0aaa52f5',
       }
-    ],
-    [
-      '@vuepress/plugin-google-analytics',
-      {
-        'ga': 'G-BWGLYWG03M'
-      }
     ]
   ],
   themeConfig: {
@@ -40,6 +34,21 @@ module.exports = {
   head: [
     ['script', { src: 'https://cdn.bootcdn.net/ajax/libs/react/17.0.2/umd/react.production.min.js', defer: true }],
     ['script', { src: 'https://cdn.bootcdn.net/ajax/libs/react-dom/17.0.2/umd/react-dom.production.min.js', defer: true }],
-    ['script', { src: 'https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js', defer: true }]
+    ['script', { src: 'https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js', defer: true }],
+    ['script', {} , `
+      var hm1
+      (function() {
+        hm1 = document.createElement("script");
+        hm1.src = "https://www.googletagmanager.com/gtag/js?id=G-BWGLYWG03M";
+        var s1 = document.getElementsByTagName("script")[0]; 
+        s1.parentNode.insertBefore(hm1, s1);
+      })();
+      hm1.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-BWGLYWG03M');
+      }
+    `]
   ]
 }
