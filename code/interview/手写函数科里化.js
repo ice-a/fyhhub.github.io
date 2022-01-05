d98c65570eb921dffe4f3486e4140f596f4ce7cf
fyhhub <1131153523@qq.com>
2022-01-05 23:27:38 +0800

    changes

--- code/interview/手写函数科里化.js
+++ code/interview/手写函数科里化.js
@@ -1,6 +1,6 @@
 function curry(fn, args) {
   args = args || [];
-  const length = fn.length
+  const length = fn.length;
   return function() {
     const allArgs = args.concat([].slice.call(arguments))
     return allArgs.length < length ? curry.call(this, fn, allArgs) : fn.apply(this, allArgs)
