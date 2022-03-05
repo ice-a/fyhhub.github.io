52622eff15eab31aa039672ea3273d148625f606
fyhhub <1131153523@qq.com>
2022-03-05 10:04:54 +0800

    changes

--- code/interview/手写防抖节流函数.js
+++ code/interview/手写防抖节流函数.js
@@ -3,7 +3,7 @@
 //   return function() {
 //     const args = arguments
 //     clearTimeout(timer)
-//     setTimeout(() => {
+//     timer = setTimeout(() => {
 //       fn.apply(this, args)
 //     }, wait)
 //   }
@@ -12,45 +12,32 @@
 // function throttle(fn, wait) {
 //   let pre = 0
 //   return function() {
-//     const now = Date.now()
+//     let now = Date.now()
 //     if (now - pre > wait) {
-//       pre = now
 //       fn.apply(this, arguments)
+//       pre = now
 //     }
 //   }
 // }
-
-// function throttle1(fn, wait) {
-//   let timer
-//   return function() {
-//     if (!timer) {
-//       timer = setTimeout(() => {
-//         clearTimeout(timer)
-//         timer = null
-//         fn.apply(this, arguments)
-//       }, wait)
-//     }
-//   }
-// }
-
-function debounce(fn, wait) {
-  let timer
-  return function() {
-    const args = arguments
-    clearTimeout(timer)
-    timer = setTimeout(() => {
-      fn.apply(this, args)
-    }, wait)
-  }
-}
 
 function throttle(fn, wait) {
-  let pre = 0
+  let pre = 0;
   return function() {
-    let now = Date.now()
+    const now = Date.now()
     if (now - pre > wait) {
       fn.apply(this, arguments)
       pre = now
     }
+  }
+}
+
+function debounce(fn, wait) {
+  let timer = null;
+  return function() {
+    if (timer) clearTimeout(timer)
+    const args = arguments
+    setTimeout(() => {
+      fn.apply(this, args)
+    }, wait)
   }
 }
