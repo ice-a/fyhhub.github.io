ea8a5f898824ad09660369f2ae8c0cf5a0e99d55
1131153523@qq.com <fanyihui@tuhu.cn>
2022-06-20 14:31:44 +0800

    feat: changes

--- code/interview/手写防抖节流函数.js
+++ code/interview/手写防抖节流函数.js
@@ -1,45 +1,10 @@
-// function debounce(fn, wait) {
-//   let timer
-//   return function() {
-//     const args = arguments
-//     clearTimeout(timer)
-//     setTimeout(() => {
-//       fn.apply(this, args)
-//     }, wait)
-//   }
-// }
-
-// function throttle(fn, wait) {
-//   let pre = 0
-//   return function() {
-//     const now = Date.now()
-//     if (now - pre > wait) {
-//       pre = now
-//       fn.apply(this, arguments)
-//     }
-//   }
-// }
-
-// function throttle1(fn, wait) {
-//   let timer
-//   return function() {
-//     if (!timer) {
-//       timer = setTimeout(() => {
-//         clearTimeout(timer)
-//         timer = null
-//         fn.apply(this, arguments)
-//       }, wait)
-//     }
-//   }
-// }
-
 function debounce(fn, wait) {
   let timer
   return function() {
-    const args = arguments
-    clearTimeout(timer)
+    if (timer) clearTimeout(fn)
     timer = setTimeout(() => {
-      fn.apply(this, args)
+      fn.apply(this, arguments)
+      timer = null
     }, wait)
   }
 }
@@ -47,7 +12,7 @@ function debounce(fn, wait) {
 function throttle(fn, wait) {
   let pre = 0
   return function() {
-    let now = Date.now()
+    const now = Date.now()
     if (now - pre > wait) {
       fn.apply(this, arguments)
       pre = now
